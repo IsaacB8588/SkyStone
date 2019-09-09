3bca57605d97a3d599d9963752592cf6dd6ec0a7
Isaac Blandin Mac <myung0966@gmail.com>
2019-09-09 18:28:49 -0500

    added basic robot base

---
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotBase.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public abstract class RobotBase extends LinearOpMode {
+
+    public abstract void initRobot (RobotRunType robotRunType);
+
+}
---
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+public abstract class RobotHardware extends RobotBase {
+
+    @Override
+    public void initRobot (RobotRunType robotRunType){
+
+    }
+
+}
